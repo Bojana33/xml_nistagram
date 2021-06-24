7f577aa5d5d9d48461b147ad09559582756f9366
Bojana33 <bojana.todorovic12@gmail.com>
2021-06-24 20:28:01 +0200

    user-service crud operations implemented

--- nistagram_microservices/user-service/src/main/java/user/userservice/Service/UserService.java
+++ nistagram_microservices/user-service/src/main/java/user/userservice/Service/UserService.java
@@ -1,4 +1,11 @@
 package user.userservice.Service;
 
+import user.userservice.Model.User;
+
 public interface UserService {
+
+    User create(User user) throws Exception;
+    User update(User user) throws Exception;
+    void delete(Long id);
+    User findOne(Long id);
 }
