a1fa88fd11b811ede93ec9126e046414f18ad4fd
pc <miroslav9gag@gmail.com>
2021-06-24 23:23:19 +0200

    Uradjena skelet post-servisa i uradjen backend za funkcionalnost 1.1 TODO frontend za f-nost 1.1

--- nistagram_microservices/post-service/src/main/java/post/postservice/Controller/PostController.java
+++ nistagram_microservices/post-service/src/main/java/post/postservice/Controller/PostController.java
@@ -8,3 +8,4 @@ public class PostController {
     @Autowired
     public PostService postService;
 }
+
--- nistagram_microservices/post-service/src/main/java/post/postservice/Repository/PostRepository.java
+++ nistagram_microservices/post-service/src/main/java/post/postservice/Repository/PostRepository.java
@@ -1,5 +1,15 @@
 package post.postservice.Repository;
 
-public interface PostRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import post.postservice.Model.Post;
 
+import java.util.List;
+
+@Repository
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUsernameOrderByCreatedAtDesc(String username);
+    List<Post> findByIdInOrderByCreatedAtDesc(List<Long> ids);
 }
+
