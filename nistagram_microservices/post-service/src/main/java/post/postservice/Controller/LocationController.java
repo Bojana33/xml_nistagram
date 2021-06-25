f0aecdde9bd1242c6b2c1d64517a17ae7ff68113
Dijana <dijana.mukic@synechron.com>
2021-06-25 10:39:20 +0200

    Location (find by name)

--- nistagram_microservices/post-service/src/main/java/post/postservice/Controller/LocationController.java
+++ nistagram_microservices/post-service/src/main/java/post/postservice/Controller/LocationController.java
@@ -1,10 +1,24 @@
 package post.postservice.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import post.postservice.Model.Location;
 import post.postservice.Service.LocationService;
 
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/location")
 public class LocationController {
 
     @Autowired
     public LocationService locationService;
+
+    @GetMapping
+    public ResponseEntity<?> findByName(@PathVariable("name") List<String> names) {
+        List<Location> locations = locationService.findByName(names);
+        return ResponseEntity.ok(locations);
+    }
+
 }
--- nistagram_microservices/post-service/src/main/java/post/postservice/Service/impl/LocationServiceImpl.java
+++ nistagram_microservices/post-service/src/main/java/post/postservice/Service/impl/LocationServiceImpl.java
@@ -1,6 +1,19 @@
 package post.postservice.Service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import post.postservice.Model.Location;
 import post.postservice.Service.LocationService;
 
+import java.util.List;
+
+@Service
 public class LocationServiceImpl implements LocationService {
+
+    @Autowired
+    private LocationService locationService;
+
+    public List<Location> findByName(List<String> names) {
+        return locationService.findByName(names);
+    }
 }
