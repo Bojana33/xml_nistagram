1f5df116a61e366e6d6c388c6d23f9a4e1fb055c
boris <borissrbotnjak@gmail.com>
2021-06-26 16:25:01 +0200

    Adding src files that did't added last time

---
+++ nistagram_microservices/validation-request-service/src/test/java/com/xml/validationrequestservice/ValidationRequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xml.validationrequestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationRequestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
