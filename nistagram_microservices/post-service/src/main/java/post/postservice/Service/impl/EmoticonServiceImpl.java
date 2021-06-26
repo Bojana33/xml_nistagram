754887f0a30b85667d85221fa42f678b8d27019c
Dijana <dijana.mukic@synechron.com>
2021-06-26 15:19:47 +0200

    Review ofliked and disliked posts

---
+++ nistagram_microservices/post-service/src/main/java/post/postservice/Service/impl/EmoticonServiceImpl.java
@@ -0,0 +1,41 @@
+package post.postservice.Service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import post.postservice.Model.EmoticonType;
+import post.postservice.Model.Post;
+import post.postservice.Repository.EmoticonRepository;
+import post.postservice.Service.EmoticonService;
+
+import java.util.List;
+
+@Service
+public class EmoticonServiceImpl implements EmoticonService {
+
+    @Autowired
+    private EmoticonRepository emoticonRepository;
+
+    public List<Post> findByLike(EmoticonType emoticonType) throws Exception{
+        List<Post> likedPosts = this.emoticonRepository.getByLike(emoticonType);
+        if (emoticonType == emoticonType.LIKE) {
+            return likedPosts;
+        }
+        else if(likedPosts == null) {
+            throw new Exception("There are no liked posts.");
+        }
+        return null;
+
+    }
+
+    public List<Post> findByDislike(EmoticonType emoticonType) throws Exception{
+        List<Post> dislikedPosts = this.emoticonRepository.getByDislike(emoticonType);
+        if(emoticonType == EmoticonType.DISLIKE){
+            return dislikedPosts;
+        }
+        if(dislikedPosts == null) {
+            throw new Exception("There are no disliked posts.");
+        }
+        return null;
+    }
+
+}
