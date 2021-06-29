880e3a2465feb10a45b4624a0aa941ac117fc0a9
Dijana <dijana.mukic@synechron.com>
2021-06-29 20:09:51 +0200

    Some fixes

--- nistagram_microservices/graph-service/src/main/java/graph/graphservice/Controller/UserController.java
+++ nistagram_microservices/graph-service/src/main/java/graph/graphservice/Controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
+@RequestMapping(value = "/")
 public class UserController {
     
     private UserService userService;
