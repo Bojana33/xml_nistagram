fe713ae5e2ac3bfb874e79be0c35c7d92a435622
Miroslav <miroslav9gag@gmail.com>
2021-09-05 13:47:30 +0200

    Upload post and view posts from an user while being unregistered

--- nistagram_microservices/photo-service/src/main/java/photo/photoservice/Controller/ImageController.java
+++ nistagram_microservices/photo-service/src/main/java/photo/photoservice/Controller/ImageController.java
@@ -1,7 +1,15 @@
 package photo.photoservice.Controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import photo.photoservice.Service.ImageService;
 
-@Controller
+@RestController
 public class ImageController {
+    @Autowired
+    private ImageService imageService;
+
+
 }
